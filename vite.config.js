476cfa0012bcf0ddfcdde94e9b2a766c492a3003
goodtimes668 <23412847+goodtimes668@users.noreply.github.com>
2026-06-03 05:08:58 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    proxy: {
-      '/.netlify/functions': {
-        target: 'http://localhost:8888',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+import { defineConfig } from 'vite'  
+import react from '@vitejs/plugin-react'  
+  
+export default defineConfig({  
+  plugins: [react()],  
+  root: '.',  
+  build: { outDir: 'dist', sourcemap: false },  
+}) 
